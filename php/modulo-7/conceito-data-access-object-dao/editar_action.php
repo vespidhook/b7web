cee168850f65f7bb8f06d8dfe248db1151b27de2
Bruno Alves <vespidhook@gmail.com>
2023-04-08 21:06:19 -0300

    conceito-data-access-object-dao

---
+++ php/modulo-7/conceito-data-access-object-dao/editar_action.php
@@ -0,0 +1,22 @@
+<?php
+require 'config.php';
+
+$id = filter_input(INPUT_POST, 'id');
+$nome = filter_input(INPUT_POST, 'nome');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+if ($id && $nome && $email) {
+
+  $sql = $pdo->prepare("UPDATE usuarios SET nome = :nome, email = :email WHERE id = :id");
+  $sql->bindValue(':nome', $nome);
+  $sql->bindValue(':email', $email);
+  $sql->bindValue(':id', $id);
+  $sql->execute();
+
+  header("Location: index.php");
+  exit;
+  
+  } else {
+    header("Location: adicionar.php");
+    exit;
+  }

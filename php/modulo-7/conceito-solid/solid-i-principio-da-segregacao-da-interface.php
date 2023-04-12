ec9438cb64361736f8124fa235ca6e951ebe2516
Bruno Alves <vespidhook@gmail.com>
2023-04-12 19:21:15 -0300

    ...

--- php/modulo-7/conceito-solid/solid-i-principio-da-segregacao-da-interface.php
+++ php/modulo-7/conceito-solid/solid-i-principio-da-segregacao-da-interface.php
@@ -1,1 +1,22 @@
 <?php
+interface Aves {
+  public function setLocation($lat, $lng);
+  public function render();
+}
+interface AvesQueVoam extends Aves {
+  public function setAltitude($alt);
+}
+
+class Papagaio implements AvesQueVoam {
+  public function setLocation($lat, $lng) {}
+  public function setAltitude($alt) {}
+  public function render() {}
+}
+class Pinguim implements Aves {
+  public function setLocation($lat, $lng) {}
+  public function render() {}
+}
+
+function xyz(Aves $ave) {
+  $ave->render();
+}
